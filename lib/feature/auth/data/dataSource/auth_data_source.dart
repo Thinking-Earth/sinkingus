8b6ee8b137c06d0d6d4346f7f0aea431b8fa2f23
toass <taoss3932@gmail.com>
2024-02-12 05:05:18 +0900

    feat: session refresh && build socket

--- lib/feature/auth/data/dataSource/auth_data_source.dart
+++ lib/feature/auth/data/dataSource/auth_data_source.dart
@@ -4,6 +4,21 @@ import 'package:sinking_us/feature/auth/data/model/user_info_model.dart';
 
 class AuthDataSource {
 
+  // session
+  Future<Session> refreshSession({required Session session}) {
+    return NakamaService().refreshNakamaSession(session: session);
+  }
+
+  Future<void> logOutSession({required Session session}) async {
+    await NakamaService().logOutNakamaSession(session: session);
+  }
+
+  // socket
+  NakamaWebsocketClient buildSocket({required String token}) {
+    return NakamaService().buildNakamaWebSocket(token: token);
+  }
+
+  // auth
   Future<Session> socialLoginWithGoogle({required UserInfoModel userInfo}) async {
     return await NakamaService().signInWithCustom(userInfo: userInfo);
   }
