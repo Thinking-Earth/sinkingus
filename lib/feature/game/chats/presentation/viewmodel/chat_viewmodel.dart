42541198495edf8c547a08bf36715793d3827143
toass <taoss3932@gmail.com>
2024-04-13 23:57:55 +0900

    fix : package dependencies

--- lib/feature/game/chats/presentation/viewmodel/chat_viewmodel.dart
+++ lib/feature/game/chats/presentation/viewmodel/chat_viewmodel.dart
@@ -1,7 +1,5 @@
-import 'package:audioplayers/audioplayers.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:easy_localization/easy_localization.dart';
-import 'package:flame_audio/flame_audio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
